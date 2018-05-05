158b4326096d7bd0eb2070f34b3e6d3d9b399285
kyr <coralisigns@gmail.com>
2018-05-06 00:17:27 +0300

    my first files upload

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * The base MySQL settings of Osclass
+ */
+define('MULTISITE', 0);
+
+/** MySQL database name for Osclass */
+define('DB_NAME', 'user8712913104_ola_paros_db2');
+
+/** MySQL database username */
+define('DB_USER', 'user8_paros');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'DQ5N2VhZmY@1');
+
+/** MySQL hostname */
+define('DB_HOST', 'db35.grserver.gr:3306');
+
+/** Database Table prefix */
+define('DB_TABLE_PREFIX', 'oc_');
+
+define('REL_WEB_URL', '/olaparos/');
+
+define('WEB_PATH', 'https://www.digitalvision.gr/olaparos/');
+
+?>
