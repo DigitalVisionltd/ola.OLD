ae8ee81c971af672e1f497e02b6b1aedac38e4e9
MrKyr <kyr@designisdesign.eu>
2018-05-09 22:32:40 +0300

    update config.php to match syros pass

--- config.php
+++ config.php
@@ -5,10 +5,10 @@
 define('MULTISITE', 0);
 
 /** MySQL database name for Osclass */
-define('DB_NAME', 'user8712913104_ola__db');
+define('DB_NAME', 'user8712913104_ola_syros_db');
 
 /** MySQL database username */
-define('DB_USER', 'user8_');
+define('DB_USER', 'user8_syros');
 
 /** MySQL database password */
 define('DB_PASSWORD', 'TdiYmYzZjM@1');
@@ -21,5 +21,5 @@ define('DB_TABLE_PREFIX', 'oc_');
 
 define('REL_WEB_URL', '/ola/');
 
-define('WEB_PATH', 'https://www.ola.gr/ola/');
+define('WEB_PATH', 'https://www.olasyros.gr/ola/');
 
