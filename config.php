c9822640b600d3334f5e188ced712a3c5efbcf72
kyr <coralisigns@gmail.com>
2018-05-13 14:00:06 +0300

    change sample values in config.php file

--- config.php
+++ config.php
@@ -6,22 +6,22 @@
 define('MULTISITE', 0);
 
 /** MySQL database name for Osclass */
-define('DB_NAME', 'ola_skeleton');
+define('DB_NAME', '');
 
 /** MySQL database username */
-define('DB_USER', 'reporter');
+define('DB_USER', '');
 
 /** MySQL database password */
-define('DB_PASSWORD', '1000');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', '');
 
 /** Database Table prefix */
 define('DB_TABLE_PREFIX', 'oc_');
 
 define('REL_WEB_URL', '/ola/');
 
-define('WEB_PATH', 'http://designisdesign.eu/ola/');
+define('WEB_PATH', 'http://.../ola/');
 
 ?>
